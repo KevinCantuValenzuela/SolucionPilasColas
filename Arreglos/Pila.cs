062c962a0919019585893cf3304e85c26aabd8f8
Kevin de Jes�s Cantu Valenzuela <Kevin_Cantu23@unikino.edu.mx>
2024-03-23 11:02:15 -0700

    agregando funcion de validacion si esta lleno

---
+++ Arreglos/Pila.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Pila
+    {
+        private string[] _arreglo;
+        private int _tope;
+        private int _max;
+
+
+        public Pila(int elemntos)
+        {
+            _arreglo = new string[elemntos];
+            _tope = 0;
+            _max = _arreglo.Length - 1;
+
+        }
+
+        private bool EstaLleno()
+        {
+            if (_tope > _max)
+            {
+                return true;
+
+
+            }
+            else
+        }
+                return false;
+
+            }
+
+        }
+
+}
+}
--- PilasColas/Program.cs
+++ PilasColas/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Arreglos.Pila pila = new Arreglos.Pila(10);
